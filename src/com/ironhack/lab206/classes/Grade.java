0e0c678aa1185e8a9b3ea52f307ebf3f28d8e3ae
Lucia de Haro <elle.dharo@gmail.com>
2022-06-14 19:44:53 +0200

    classes

--- src/com/ironhack/lab206/classes/Grade.java
+++ src/com/ironhack/lab206/classes/Grade.java
@@ -1,2 +1,15 @@
-package com.ironhack.lab206.classes;public class Grade {
+package com.ironhack.lab206.classes;
+
+import java.util.Map;
+
+public class Grade {
+
+    public static Map<String, Student> gradeIncrease (Map<String, Student> studentMap){
+
+        for(Student student : studentMap.values()) student.setGrade(student.getGrade()*1.1);
+
+        return studentMap;
+    }
+
+
 }
--- src/com/ironhack/lab206/main/Main.java
+++ src/com/ironhack/lab206/main/Main.java
@@ -1,2 +1,27 @@
-package com.ironhack.lab206.main;public class Main {
+package com.ironhack.lab206.main;
+
+import com.ironhack.lab206.classes.Student;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.ironhack.lab206.classes.Grade.gradeIncrease;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Betty Rizzo", 55);
+        Student s2 = new Student("Sandra Dee", 65);
+        Student s3 = new Student("Danny Zuko", 99);
+
+        Map<String, Student> studentMap = new HashMap<>();
+        studentMap.put("Betty Rizzo", s1);
+        studentMap.put("Sandra Dee", s2);
+        studentMap.put("Danny Zuko", s3);
+
+        gradeIncrease(studentMap);
+
+        System.out.println(studentMap.values());
+
+    }
 }
